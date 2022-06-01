a7162264f2d00f432fa71171249ecf7bc1f128a3
kangmin <rlarkdals579@naver.com>
2022-06-01 21:04:35 +0900

    Order Service (Update)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,23 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
+    /**
+     * 추상, 구현(FixDiscountPolicy)클래스 둘 다 의존을 하므로, DIP 위반
+     * 또한, FixDiscountPolicy를 RateDiscountPolicy로 변경하는 순간, OrderServiceImpl의 소스코드를
+     * 변경해야 하므로, OCP를 위반
+     */
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy; //구현체의 객체가 없어서 NullPointException 발생
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
