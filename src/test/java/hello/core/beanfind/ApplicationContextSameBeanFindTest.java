ecded2e54ea1fedb414f11fed7703cc775dcfe89
kangmin <rlarkdals579@naver.com>
2022-06-07 23:27:27 +0900

    스프링 빈 조회 - 상속 관계, XML

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -29,8 +29,8 @@ public class ApplicationContextSameBeanFindTest {
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
-    void findBeanByName(){
-        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);
+    void findBeanByName() {
+        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
@@ -44,7 +44,7 @@ public class ApplicationContextSameBeanFindTest {
 
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
-    void finAllBeanByType(){
+    void finAllBeanByType() {
         Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + "value = " + beansOfType.get(key));
